1a35c18d3ddfd6990694cb8452b975683eb61d42
DAMH1 <81368782+damh@users.noreply.github.com>
2022-05-04 18:03:35 -0500

    Add files via upload

---
+++ controladores/r_programas.php
@@ -0,0 +1,24 @@
+
+    <?php
+
+
+    include("../class/programas.php");
+    $ficha = $_POST[ 'ficha' ];
+    $programa = $_POST[ 'programa' ];
+    $cp= $_POST[ 'cp' ];
+    $nivel=$_POST['nivel'];
+    $t_formacion=$_POST['t_formacion'];
+  
+    if ($ficha == true && $programa == true && $cp == true && $nivel == true && $t_formacion == true)
+    {
+      
+        insertar_programas::insertar($ficha, $programa,$cp, $nivel, $t_formacion);
+        
+       
+        
+    }
+    else{
+       
+      
+       header( "location: ../vistas/programas.php" );
+    }
