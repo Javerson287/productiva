2174cee3eb44bae32704741ad7b6aba39ab64d7b
DAMH1 <81368782+damh@users.noreply.github.com>
2022-05-05 14:51:12 -0500

    Add files via upload

--- class/programas.php
+++ class/programas.php
@@ -1,24 +1,27 @@
 <?php
 include ('conexion.php');
 
-class insertar_programa extends Conex
+class insertar_programas extends Conex
 {
 
-    static function insertar ($ficha, $nom_programa)
+    static function insertar ($ficha, $programa,$cp, $nivel, $t_formacion)
         {
-            
+           
             $conexion = Conex::conectar();
-            $sql= " insert into programas (ficha, nom_programa) values ('$ficha', '$nom_programa')";
+            $sql= "INSERT INTO `programas` (`ficha`, `n_programa`, `cantidad_aprendizes`, `id_nivel`,`borrar`,`t_formacion`) VALUES ('$ficha', ' $programa',$cp, ' $nivel', '1','$t_formacion');";
             $resultado = $conexion->query($sql);
-            //echo $sql;
             if ($conexion ->affected_rows > 0)
             {
                 
-                echo '<script language="javascript">alert("tus datos se guardaron");window.location.href="../vistas/agregar_programa.php";</script>';
+                
+                echo '<script language="javascript">alert("Tus datos se guardaron");window.location.href="../vistas/programas.php";</script>';
             }
             else
             {
-                echo '<script language="javascript">alert("tus datos no se guardaron");window.location.href="../vistas/agregar_programa.php";</script>';
+                
+                
+                echo '<script language="javascript">alert("Tus datos no se guardaron");window.location.href="../vistas/programas.php";</script>';
+                
                 
                 
             }
