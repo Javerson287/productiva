0fa5e25155d589c2e12eaa9c8e168e925943ff39
DAMH1 <81368782+damh@users.noreply.github.com>
2022-05-05 14:54:34 -0500

    Add files via upload

--- class/eliminar.php
+++ class/eliminar.php
@@ -37,3 +37,16 @@ function editar3($tabla, $id_campo, $id,$campo,$campo1,$profecion)
      return mysqli_query($conect,$sql);
     
 }
+function editar4($ficha_i, $ficha_f, $programa, $cp, $nivel, $formacion)
+{
+    
+    $conect = Conex::conectar();
+    $sql = "UPDATE programas SET ficha = '$ficha_f', n_programa = '$programa', cantidad_aprendizes= '$cp', id_nivel='$nivel', t_formacion='$formacion' WHERE ficha = '$ficha_i'; ";
+    echo $sql;
+    
+
+  
+     return mysqli_query($conect,$sql);
+    
+}
+
